8ed1353327da36c54a4f2c4cc12a5c97275559f5
syaw0 <siamhb7@protonmail.com>
2024-03-13 03:26:49 +0330

    .

---
+++ scanner/static/bbot/BB/2024_02_24_211414/filedownload/20240301_1250_21_https-sandbox-balatarin-com-assets-turbo-rails-2e9c86324e5a41fb6a21cba0dd8c1c4300c87491ba64a1ba2f07d5afc3dae847.js
@@ -0,0 +1,23 @@
+class Sample{
+
+
+ constructor(){
+
+this.name = 'smaple_class'
+}
+
+
+get name(){
+return this.name
+}
+
+
+set name(newName){
+this.name = newName
+}
+
+
+
+
+
+}
---
+++ scanner/static/bbot/BB/2024_02_24_211414/filedownload/20240301_1300_38_https-sandbox-balatarin-com-443-assets-application-d675ec0b833a05f5a02c49c854aae636b00308cc264ff3b926aae4edba56e0da.js
@@ -0,0 +1,32 @@
+try {
+   function Captcha(){
+      const getRecaptchaToken = () => {
+      return new Promise((resolve, reject) => {
+      try {
+      if (window.grecaptcha && typeof window.grecaptcha.execute === "function") {
+        grecaptchaExecute(window.grecaptcha.execute);
+      } else {
+        window.grecaptcha.ready(async () => {
+          grecaptchaExecute(window.grecaptcha.execute);
+        });
+      }
+      // grecaptcha execute action
+      async function grecaptchaExecute(ExecuteAction) {
+        const captchaToken = await ExecuteAction(
+          APIKEY, 
+          {
+            action: "submit",
+          }
+        );
+        return resolve(captchaToken);
+      }
+    } catch (error) {
+      return reject(error);
+    }
+  });
+};
+    }
+ } catch (error) {
+	console.log(error)
+  Captcha();
+ }
